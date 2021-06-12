157cff04aac8e811510d1e2f29a40b843ec21bb7
madhav06 <mnandan06@gmail.com>
2021-06-12 13:07:59 +0530

    Adding styles

--- pizzabites/src/pages/_app.js
+++ pizzabites/src/pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Link from 'next/link';
+import styles from '../styles/MyApp.module.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return(
+        <div>
+          <nav className={styles.navbar}> 
+            <ul className={styles.navbarContactBox}> 
+              <Link href="/">
+                <a>
+                  <div>
+                    <img src="#" alt="brand" />
+                    <p> PizzaBites </p>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/"><a className={styles.navbarContact}><li> Home</li></a></Link>
+              <Link href="/about"><a className={styles.navbarContact}><li> About</li></a></Link>
+            </ul>
+          </nav>
+          <div className={styles.container}>
+            <Component {...pageProps} />
+          </div>
+        </div>
+    )
 }
 
 export default MyApp

0efe24fa5340b39793068b9f7f8ab8575455a7c3
madhav06 <mnandan06@gmail.com>
2021-06-15 17:05:09 +0530

    Adding models, views, urls

--- pizzabites_backend/pizza/models.py
+++ pizzabites_backend/pizza/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.CharField(max_length=500)
+    toppings = ArrayField(
+        models.CharField(max_length=100)
+    )
+    image = models.URLField()
+    price = models.FloatField()
+
+    class Meta:
+        ordering = ['name',]
+
+    def __str__(self):
+        if self is None:
+            return f'None'
+        else:
+            return self.name
+

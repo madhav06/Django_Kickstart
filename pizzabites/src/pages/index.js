1946a3e4c56c9a9463964bfb6161142d3a1b82a2
madhav06 <mnandan06@gmail.com>
2021-06-16 08:55:19 +0530

    Updating Frontend, Updating Home.js, index.js and slug.js

--- pizzabites/src/pages/index.js
+++ pizzabites/src/pages/index.js
@@ -4,12 +4,23 @@ import Layout from '../components/Layout/Layout';
 import Home from '../components/Home/Home';
 
 
-export default function home() {
+export default function home({pizzas}) {
     return (
         
         <Layout>
-            <Home />
+            <Home pizzas={pizzas} />
         </Layout>
         
     )
+}
+
+export const getStaticProps = async ({params}) => {
+    const response = await fetch('http://localhost:8000/pizza/');
+    const pizzas = await response.json();
+
+    return {
+        props: {
+            pizzas,
+        }
+    }
 }
